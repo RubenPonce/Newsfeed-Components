c5036f328f270d1cb840293874cc9edac51f9a7d
Ruben Ponce <ruben_ponce1@yahoo.com>
2019-04-10 15:50:40 -0700

    formatting

--- Article/Article.js
+++ Article/Article.js
@@ -1,12 +1,11 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
-  constructor(domElement,index) {
+  constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
-    this.index = index;
     // create a reference to the ".expandButton" class. 
-    this.expandButton = document.querySelectorAll('.expandButton')[index];
+    this.expandButton = domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
@@ -28,4 +27,4 @@ class Article {
 */
 
 let articles = document.querySelectorAll('.article');
-articles.forEach((article,index)=> new Article(article,index));
+articles.forEach((article)=> new Article(article));
